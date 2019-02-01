7ed4fad21ceefc3ea056c174afab6f365aadb4a5
julianamtyler <julianam.tyler@gmail.com>
2019-01-31 19:56:03 -0500

    added switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import Welcome from './Pages/Welcome/Welcome';
 import AboutUs from './Pages/AboutUs/AboutUs';
 import Blogs from './Pages/Blogs/Blogs';
@@ -7,7 +7,7 @@ import Services from './Pages/Services/Services';
 import Testimonials from './Pages/Testimonials/Testimonials';
 import ContactUs from './Pages/ContactUs/ContactUs';
 import Navigation from './Components/Navigation';
-// import Footer from './Components/Footer';
+import Footer from './Components/Footer';
 
 
 const App = () => (
@@ -21,14 +21,15 @@ const App = () => (
         <button className="navButton"><Link className="linkStyle" to={`/testimonials`}>Testimonials</Link></button>
         <button className="navButton"><Link className="linkStyle" to={`/contactus`}>Contact Us</Link></button>
       </Navigation>
+      <Switch>
       <Route exact path='/' component={Welcome} />
       <Route exact path='/aboutus'  component={AboutUs} />
       <Route exact path='/blogs' component={Blogs} />
       <Route exact path='/services' component={Services} />
       <Route exact path='/testimonials' component={Testimonials} />
       <Route exact path='/contactus' component={ContactUs} /> 
-      
-    {/* <Footer/> */}
+      </Switch>
+    <Footer/>
     </div>
   </BrowserRouter>
 )
