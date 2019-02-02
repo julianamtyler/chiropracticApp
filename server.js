5c0cc8a43a8f57e1802ab5bccda8a44bbf8d9c55
julianamtyler <julianam.tyler@gmail.com>
2019-02-02 03:17:30 -0500

    didnt save repush changes

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join('build', 'index.html'));
-  });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join('build', 'index.html'));
+//   });
+// }
 
 // Add routes, both API and view
 require('./routes/api-routes')(app);
