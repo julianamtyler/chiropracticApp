325629ee1b09662c5fbfc06e4da1091f527c9539
julianamtyler <julianam.tyler@gmail.com>
2019-02-02 03:16:30 -0500

    changed server file back to original

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './footerStyle.css';
-import { Link } from 'react-router-dom';
 const Footer = () => (
     <div className="footer">
         Chiropractic Wellness by Juliana Tyler- © Copyright 2019 All Rights Reserved. 
