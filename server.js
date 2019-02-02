b6572f08138208fa999a2ac0fd58a067290bccb9
julianamtyler <julianam.tyler@gmail.com>
2019-02-02 03:28:31 -0500

    changed back to original server.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
 
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static('client/build'));
