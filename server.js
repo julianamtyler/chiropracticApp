7d35f58ad4ddc988d3e4a5b9111815c37cb0f7f9
julianamtyler <julianam.tyler@gmail.com>
2019-02-02 03:12:03 -0500

    trying a new modification to server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // }
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.join('build', 'index.html'));
   });
