b447fdb4d785d14d5ce37300bb6db9fdf1719d4f
julianamtyler <julianam.tyler@gmail.com>
2019-02-07 21:17:03 -0500

    some styling done

--- client/src/Pages/Testimonials/ReviewForm.js
+++ client/src/Pages/Testimonials/ReviewForm.js
@@ -4,9 +4,9 @@ const ReviewForm = (props) => (
   
        <div className="reviewForm">
        <form>
-           <input name="name" value={props.nameVal} placeholder="Your First Name and First letter of Last name" onChange={props.handleChange} className="nameInput" /><br/>
+           <input name="name" value={props.nameVal} placeholder="Your First Name & First letter of Last Name" onChange={props.handleChange} className="nameInput" /><br/>
            <input name="email" value={props.emailVal}  placeholder="What is your email?" onChange={props.handleChange} className="emailInput" /><br/>
-           <input name="comment" value={props.commentVal} placeholder="Leave your review here" onChange={props.handleChange} className="commentInput"/><br/>
+           <textarea name="comment" value={props.commentVal} placeholder="Leave your review here" onChange={props.handleChange} className="commentInput"></textarea><br/>
            <button  className="formButton" onClick={props.submitReview}>Submit</button>
        </form>
        </div>
